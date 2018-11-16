a8c3965786029f08a678292baae387d4f080f04f
jmirtil <jmirt001@fiu.edu>
2018-11-16 00:39:40 -0500

    Redesign app

--- app/src/main/java/com/caribagospel/caribagospel/PlayStreams.java
+++ app/src/main/java/com/caribagospel/caribagospel/PlayStreams.java
@@ -1,13 +1,14 @@
 package com.caribagospel.caribagospel;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ToggleButton;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,7 +17,6 @@ import java.util.Map;
 public class PlayStreams extends AppCompatActivity {
 
     protected MediaPlayer mp;
-    protected boolean isPlaying;
 
     protected Map<Integer, String> streamMap = new HashMap<>();
 
@@ -26,7 +26,6 @@ public class PlayStreams extends AppCompatActivity {
         setContentView(R.layout.activity_play_streams);
 
         setUpStream();
-        isPlaying = false;
         ViewGroup layout = findViewById(R.id.layout_container_buttons);
         for (int i = 0; i < layout.getChildCount(); i++) {
 
@@ -41,17 +40,17 @@ public class PlayStreams extends AppCompatActivity {
             }
         }
 
-        final ToggleButton play = findViewById(R.id.playButton);
+        final Button play = findViewById(R.id.playButton);
         play.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                resumeStream(play);
+                resumeStream();
             }
         });
 
         Button stop = findViewById(R.id.stopButton);
         stop.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                stopStream(play);
+                stopStream();
             }
         });
 
@@ -59,7 +58,7 @@ public class PlayStreams extends AppCompatActivity {
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                closeApp();
+                shutDown();
             }
         });
     }
@@ -68,61 +67,46 @@ public class PlayStreams extends AppCompatActivity {
      * Setup links to the stream for each button in the app.
      */
     protected void setUpStream(){
-        streamMap.put(R.id.button, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button4, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button5, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button6, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button7, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
+        streamMap.put(R.id.button, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button2, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button3, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button4, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button5, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button6, "http://www.all-birds.com/Sound/western%20bluebird.wav");
+        streamMap.put(R.id.button7, "http://www.all-birds.com/Sound/western%20bluebird.wav");
         streamMap.put(R.id.button8, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button9, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button15, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button16, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button17, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button18, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button19, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button20, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button21, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button22, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
-        streamMap.put(R.id.button23, "https://samcloud.spacial.com/api/listen?sid=92412&m=sc&rid=166620");
     }
 
     /**
      * This is the action handler for the play button. It toggles between play and pause.
-     * @param playButton
      */
-    public void resumeStream(ToggleButton playButton) {
+    public void resumeStream() {
         if (mp != null) {
-            if(!isPlaying)
-            {
-                mp.start();
-                isPlaying = true;
-                playButton.setChecked(true);
-            }
-            else
-            {
-                mp.pause();
-                isPlaying = false;
-                playButton.setChecked(false);
-            }
+            mp.start();
+            Button play = findViewById(R.id.playButton);
+            play.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+            play.setEnabled(false);
         }
     }
 
-    public void stopStream(ToggleButton playButton) {
+    public void stopStream() {
         if (mp != null) {
             mp.pause();
-            isPlaying = false;
-            playButton.setChecked(false);
         }
+
+        Button play = findViewById(R.id.playButton);
+        play.getBackground().setColorFilter(null);
+        play.setEnabled(true);
     }
 
-    public void closeApp() {
+    public void shutDown() {
         if (mp != null) {
             mp.stop();
             mp.release();
             mp = null;
-            isPlaying = false;
         }
-        closeApp();
+        this.finish();
+        System.exit(0);
     }
 
     public void play(String url) {
@@ -141,8 +125,10 @@ public class PlayStreams extends AppCompatActivity {
             });
             mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mp.setDataSource(url);
-            mp.prepare();
-            isPlaying = true;
+            mp.prepareAsync();
+
+            Button play = findViewById(R.id.playButton);
+            play.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
             mp.start();
         } catch (IOException e) {
             e.printStackTrace();
